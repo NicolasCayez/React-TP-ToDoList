6f3be16e1edd39e3b485fe34427913e1520686cd
NicolasCayez <nicolascayez@gmail.com>
2025-01-27 15:48:17 +0100

    TP fini

--- src/App.jsx
+++ src/App.jsx
@@ -3,33 +3,108 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+//? Partie 3 et 4
+const TODOS = [
+  {todo:"Devoirs", date:"2025-01-12", checked:false},
+  {todo:"Lessive", date:"2025-01-14", checked:true},
+  {todo:"Mettre la table", date:"2025-01-18", checked:false},
+  {todo:"Cuisine", date:"2025-02-02", checked:false},
+  {todo:"Vaisselle", date:"2025-02-11", checked:false}
+]
+
+export default function App() {
+  const DATE = new Date();
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <article className="todolist">
+        <h1>Ma ToDo List</h1>
+        <h2>{DATE.toLocaleString()}</h2>
+
+        {/* Partie 1
+        <ToDo /> */}
+
+        {/* Partie 2
+        <ul>
+          <ToDo todo={"Mettre la table"} date={new Date("2025-01-12").toLocaleDateString()} />
+          <ToDo todo={"Cuisine"} date={new Date("2025-02-23").toLocaleDateString()} />
+          <ToDo todo={"Vaisselle"} date={new Date("2025-04-02").toLocaleDateString()} />
+        </ul> */}
+
+        {/* Partie 3
+        <ul>
+          {TODOS.map((uneTache, index) =>
+            <ToDo key={index} todo={uneTache.todo} date={uneTache.date} />
+          )}
+        </ul> */}
+
+        {/* Partie 4 */}
+        <ul>
+          {TODOS.map((uneTache, index) =>
+            // <ToDo key={index} todo={uneTache.todo} date={uneTache.date} checked={uneTache.checked} />
+            // <ToDoTernaire key={index} todo={uneTache.todo} date={uneTache.date} checked={uneTache.checked} />
+            <ToDoAnd key={index} todo={uneTache.todo} date={uneTache.date} checked={uneTache.checked} />
+          )}
+        </ul>
+      </article>
     </>
   )
 }
 
-export default App
+//? Partie 1
+// function ToDo({todo, date}){
+//   return (
+//     <>
+//       <ul>
+//         <li>Devoirs</li>
+//         <li>Lessive</li>
+//         <li>Mettre la table</li>
+//         <li>Cuisine</li>
+//         <li>Vaisselle</li>
+//       </ul>
+//     </>
+//   )
+// }
+
+//? Partie 2 et 3
+// function ToDo({todo, date}){
+//   return (
+//     <>
+//       <li>{todo} - {date}</li>
+//     </>
+//   )
+// }
+
+//? Partie 4
+function ToDo({todo, date, checked}){
+  if (checked) {
+    return (
+      <>
+        <li className="green"><input type="checkbox" defaultChecked /><label>{todo} - {date}</label></li>
+      </>
+    )
+  }else{
+    return (
+      <>
+        <li className="orange"><input type="checkbox" /><label>{todo} - {date}</label></li>
+      </>
+    )
+  }
+}
+
+function ToDoTernaire ({todo, date, checked}) {
+  return (
+    <>
+      {checked ? <li className="green"><input type="checkbox" defaultChecked /><label>{todo} - {date}</label></li> : <li className="orange"><input type="checkbox" /><label>{todo} - {date}</label></li> }
+    </>
+  )
+}
+
+function ToDoAnd ({todo, date, checked}) {
+  return (
+    <>
+      <li className={checked ? "green" : "orange"}>{checked && <input type="checkbox" defaultChecked />}<label>{todo} - {date}</label></li>
+    </>
+  )
+}
+
